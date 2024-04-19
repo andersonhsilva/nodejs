9a3f08d5a940788101fd7a6454e41d2ded380b30
andersosn henrique <andderson.h.silva@gmail.com>
2024-04-18 21:58:06 -0300

    get list, post

--- index.js
+++ index.js
@@ -7,54 +7,21 @@ atencao: define o tipo de dado passado pelop post ao servidor node pra do tipo j
 */
 server.use(express.json());
 
-/*
-    Query Params = ?nome=anderson
-    Route Params = /curso/2
-    Request Body = { nome: 'anderson', cpf: 06926903441 }
-
-    variaveis:
-    req = dados da nossa aplicacao, query param ou dados do body
-    res = representa uma resposta pra o nosso frontend
-
-    CRUD = Create, Read, Update e Delete
-*/
-
-// query params
-server.get('/curso', (req, res) => {
-    const nome = req.query.nome;
-    return res.json({ curso: `Aprendendo: ${nome}` });
-});
-
+// CRUD = Create, Read, Update e Delete
 const cursos = ['Node JS', 'JavaScript', 'React Native'];
 
-// route params GET
+// route params GET -> listar tudo
 server.get('/cursos', (req, res) => {
     return res.json(cursos);
 });
 
-// request body -> route POST
+// request body -> POST
 server.post('/curso', (req, res) => {
-    const {name} = req.body;
-    cursos.push(name);
+    const { name } = req.body;
+    cursos.push(name); // adiciona um novo elemento no array js
     return res.json(cursos);
 });
 
-// route params
-server.get('/curso/:num', (req, res) => {
-    const {num} = req.params;
-    return res.json(cursos[num]);
-});
-
-// route GETs
-server.get('/log', () => {
-    console.log('acessou a rota mensagem no backend!');
-});
-server.get('/hello_html', (req, res) => {
-    return res.send('<h1>hello word</h1>');
-});
-server.get('/hello_json', (req, res) => {
-    return res.json({ hello: 'word' });
-});
 
 // inicia o servidor node nesta porta
 server.listen(3333);
